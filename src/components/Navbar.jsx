0316550e3c793c28b7440625026a2bd8363b6f1b
Amol <shelkeamol897@gmail.com>
2023-07-21 23:07:20 +0530

    [config]: tailwind initialized

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex-row justify-between items-center hidden sm:flex px-8">
+      <div className="relative">
+        <h1 className="text-xl">Animated slider</h1>
+      </div>
+      <div className="">
+        <ul className="flex justify-around px-4">
+          <li className="mr-2 text-lg">Home</li>
+          <li className="mr-2 text-lg">About</li>
+          <li className="mr-2 text-lg">More info</li>
+          <li className="text-lg">About us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
