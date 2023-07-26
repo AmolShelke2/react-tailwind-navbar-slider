4ad15d4ae12e74975dcf2729203ad9938c41cf59
Amol <shelkeamol897@gmail.com>
2023-07-26 21:37:09 +0530

    [feat]:navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex-row justify-between items-center hidden sm:flex px-8">
+    <div className="flex flex-row justify-between items-center px-8 bg-teal-400 py-4 text-white">
       <div className="relative">
-        <h1 className="text-xl">Animated slider</h1>
+        <h1 className="text-xl font-bold">Animated slider</h1>
       </div>
       <div className="relative">
-        <ul className="flex justify-around px-4">
+        <ul className="hidden sm:flex sm justify-around px-4 cursor-pointer">
           <li className="mr-2 text-lg">Home</li>
           <li className="mr-2 text-lg">About</li>
           <li className="mr-2 text-lg">More info</li>
