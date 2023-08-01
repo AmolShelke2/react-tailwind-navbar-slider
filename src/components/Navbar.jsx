410c7b0f778bd748f5ef45b6fa96ecdce9680f70
Amol <shelkeamol897@gmail.com>
2023-08-01 23:20:44 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <h1 className="text-xl font-bold">Animated slider</h1>
       </div>
       <div className="relative">
-        <ul className="hidden sm:flex sm justify-around px-4 cursor-pointer">
+        <ul className="hidden sm:flex justify-around px-4 cursor-pointer">
           <li className="mr-2 text-lg">Home</li>
           <li className="mr-2 text-lg">About</li>
           <li className="mr-2 text-lg">More info</li>
