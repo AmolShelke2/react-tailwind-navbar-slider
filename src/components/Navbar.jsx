b0ab1f39f68f9f0907cd3c24adffcdb4a8e32e60
Amol <shelkeamol897@gmail.com>
2023-08-02 23:05:47 +0530

    [feat]: dropdown component created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,22 +10,39 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex flex-row justify-between items-center px-8 bg-teal-400 py-4 text-white relative">
-      <div className="relative">
-        <h1 className="text-xl font-bold">Animated slider</h1>
+    <>
+      <div className="flex flex-row justify-between items-center px-8 bg-teal-400 py-4 text-white relative">
+        <div className="relative">
+          <h1 className="text-xl font-bold">Animated slider</h1>
+        </div>
+        <div className="relative">
+          <ul className="hidden sm:flex justify-around px-4 cursor-pointer">
+            <li className="mr-2 text-lg">Home</li>
+            <li className="mr-2 text-lg">About</li>
+            <li className="mr-2 text-lg">More info</li>
+            <li className="text-lg">About us</li>
+          </ul>
+        </div>
+        <div className="flex sm:hidden">
+          <AiOutlineMenu onClick={navbarHandler} />
+        </div>
       </div>
+
+      {/* Mobile navbar */}
       <div className="relative">
-        <ul className="hidden sm:flex justify-around px-4 cursor-pointer">
-          <li className="mr-2 text-lg">Home</li>
-          <li className="mr-2 text-lg">About</li>
-          <li className="mr-2 text-lg">More info</li>
+        <ul
+          className={`${
+            navBar
+              ? "flex-col justify-text py-4 items-center text-center cursor-pointer transition-[display] bg-teal-100"
+              : "hidden"
+          }`}>
+          <li className="mb-2 text-lg transition-[0.5s]">Home</li>
+          <li className="mb-2 text-lg">About</li>
+          <li className="mb-2 text-lg">More info</li>
           <li className="text-lg">About us</li>
         </ul>
       </div>
-      <div className="flex sm:hidden">
-        <AiOutlineMenu onClick={navbarHandler} />
-      </div>
-    </div>
+    </>
   );
 };
 
